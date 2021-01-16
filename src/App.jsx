c2a12e2513281e2ace6f52ad740264c9f1669e5c
hrishi045 <hrishi045@gmail.com>
2021-01-16 23:34:14 +0530

    Add an output panel and download button

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg'
 import React, { useEffect, useRef, useState } from 'react'
+import Video from 'react-video-renderer'
 import VideoEditor from './VideoEditor'
 import VideoUploadForm from './VideoUploadForm'
 
 const App = () => {
   const [video, setVideo] = useState(null)
   const ffmpeg = useRef(null)
+  const [out, setOut] = useState(null)
 
   useEffect(() => {
     async function x() {
@@ -20,16 +22,12 @@ const App = () => {
     return seconds.toFixed(2)
   }
 
-  const handleTrim = async (start, end, duration) => {
-    console.log(typeof start, typeof end, typeof duration)
-    console.log(
-      start,
-      percToFfmpegTime(start, duration),
-      end,
-      percToFfmpegTime(end, duration),
-      duration
-    )
+  const handleUpload = (videoData) => {
+    setVideo(videoData)
+    setOut(null)
+  }
 
+  const handleTrim = async (start, end, duration) => {
     const { name } = video
     await ffmpeg.current.load()
     ffmpeg.current.FS('writeFile', name, await fetchFile(video))
@@ -43,14 +41,31 @@ const App = () => {
       'output.mp4'
     )
     const data = ffmpeg.current.FS('readFile', 'output.mp4')
-    setVideo(new Blob([data.buffer], { type: 'video/mp4' }))
+    setOut(new Blob([data.buffer], { type: 'video/mp4' }))
+    setVideo(null)
   }
 
   return (
-    <div className="w-1/2 mx-auto mt-12 overflow-hidden rounded-lg shadow ">
-      <VideoEditor video={video} onTrim={handleTrim} />
-      {video === null && (
-        <VideoUploadForm onUpload={(videoData) => setVideo(videoData)} />
+    <div className="max-w-3xl mx-2 mt-12 overflow-hidden rounded-lg shadow sm:mx-auto ">
+      {out === null && <VideoEditor video={video} onTrim={handleTrim} />}
+      {video === null && <VideoUploadForm onUpload={handleUpload} />}
+      {out === null || (
+        <>
+          <div className="VideoEditor">
+            <Video src={URL.createObjectURL(out)} controls>
+              {(videoPlayer) => <>{videoPlayer}</>}
+            </Video>
+          </div>
+          <div>
+            <a
+              className="block w-full p-4 text-sm font-semibold text-center text-gray-300 capitalize bg-gray-800 cursor-pointer focus:outline-none hover:bg-gray-900"
+              href={URL.createObjectURL(out)}
+              download
+            >
+              Download
+            </a>
+          </div>
+        </>
       )}
     </div>
   )
